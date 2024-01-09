e8414489f7e39286f2f615d95aed1f4460f72dc0
Leon Forsythe <lvforsythe95@gmail.com>
2024-01-09 17:01:39 -0500

    navbar initial styling and placeholder links

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,12 +1,17 @@
 import React from "react";
-import {Route, Link} from "react-router-dom"
+import { Route, Link } from "react-router-dom";
+import classes from "./MainHeader.module.css";
+import { GiHamburgerMenu } from "react-icons/gi"
 
-export default function MainHeader(){
-    return (
-        <nav>
-            <Link>Tasks</Link>
-            <Link>Pricing</Link>
-            <Link>Terms and Conidtions</Link>
-        </nav>
-    )
+export default function MainHeader() {
+  return (
+    <nav className={classes.navbar}>
+    <button className={classes.sidebarBtn}><GiHamburgerMenu /></button>
+      <div>
+        <Link>Tasks</Link>
+        <Link>Pricing</Link>
+        <Link>Terms and Conditions</Link>
+      </div>
+    </nav>
+  );
 }
