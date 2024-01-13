ed6627c24ddc477afc3eeb391906a332fd083b75
Leon Forsythe <lvforsythe95@gmail.com>
2024-01-13 13:24:18 -0500

    layed out 3 card sections for tasks based on backlog, current, and finished. set up css grid for card sections

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,16 +2,25 @@ import React from "react";
 import classes from "./Dashboard.module.css";
 import Sidebar from "./Sidebar";
 import { useOutletContext } from "react-router-dom";
+import Card from "./Card";
 
 export default function Dashboard() {
 
-    const [isOpen] = useOutletContext()
+  const [isOpen] = useOutletContext()
 
   return (
     <main className={classes.main}>
       {(isOpen && <Sidebar/>)}
       <div className={classes.dashboard}>
-        <h1>Your Dashboard</h1>
+        <div className={classes.backlog}>
+            <Card />
+        </div>
+        <div className={classes.current}>
+            <Card />
+        </div>
+        <div className={classes.finished}>
+            <Card />
+        </div>
       </div>
     </main>
   );
