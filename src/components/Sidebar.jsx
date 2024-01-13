c06f069e9d68494fe3d08d5104536dd7e74e8c19
Leon Forsythe <lvforsythe95@gmail.com>
2024-01-12 21:45:02 -0500

    added additional placeholder elements to sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,17 @@
 import { BsFillGrid1X2Fill } from "react-icons/bs";
-import { FaRegCalendarAlt } from "react-icons/fa";
-import { MdAutoGraph } from "react-icons/md";
+import { FaRegCalendarAlt, FaRegHeart } from "react-icons/fa";
+import { MdAutoGraph, MdOutlineMessage } from "react-icons/md";
 import classes from "./Sidebar.module.css";
 
 export default function Sidebar() {
   return (
     <>
       <div className={classes.sidebar}>
-        <BsFillGrid1X2Fill />
-        <FaRegCalendarAlt />
-        <MdAutoGraph />
+        <span><BsFillGrid1X2Fill /></span>
+        <span><MdOutlineMessage /></span>
+        <span><FaRegCalendarAlt /></span>
+        <span><FaRegHeart /></span>
+        <span><MdAutoGraph /></span>
       </div>
     </>
   );
