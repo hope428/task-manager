91646fd8e247a8104a5814be3121fba5c4832b57
Leon Forsythe <lvforsythe95@gmail.com>
2024-01-16 21:12:28 -0500

    created mongoose model for tasks. seeded database with a few dummy tasks

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,1 +1,12 @@
-const db = require('../config/connection')
+const db = require("../config/connection");
+const Task = require("../models/Task");
+
+const taskData = require("./taskData.json");
+
+db.once("open", async () => {
+  await Task.deleteMany({});
+
+  const tasks = await Task.insertMany(taskData);
+  console.log("Tasks seeded! 🌱 🌱");
+  process.exit(0);
+});
