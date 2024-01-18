985753904a2876b9d1182bd96c1f805f929d6a01
Leon Forsythe <lvforsythe95@gmail.com>
2024-01-18 18:34:24 -0500

    added conditional rendering and mapping over retrieved tasks to display tasks in each priority level. added ternary operator in card component to determine priority label. passed task as prop to card component for dynamic cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import classes from "./Card.module.css"
 
 export default function Card({task}){
 
-    const importanceText = {task.importance === 1 ? "High Priority" : task.importance === 2 ? "Medium Priority" : "Low Priority"} 
+    const importanceText = task.importance === 1 ? "High Priority" : task.importance === 2 ? "Medium Priority" : "Low Priority"
 
     return (
         <div className={classes.card}>
